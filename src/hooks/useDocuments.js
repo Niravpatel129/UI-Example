3d72c07ff03212cbae445d9ce65053e3d94cc1f8
Nirav Patel <niravpatelp129@gmail.com>
2023-06-19 12:18:39 -0400

    moved moc data

--- src/hooks/useDocuments.js
+++ src/hooks/useDocuments.js
@@ -1,4 +1,4 @@
-import { accounts, defaultAccount, prefDocuments } from '../../mockData';
+import { accounts, defaultAccount, prefDocuments } from '../../static/mockData';
 
 export default function useDocuments() {
   return { accounts, prefDocuments, defaultAccount: defaultAccount.data.default_accounts[0] };
